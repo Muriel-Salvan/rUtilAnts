1086b373d562fe3df5c2198a2074d3b1c1affaf2
Muriel Salvan <MurielSalvan@users.sourceforge.net>
2009-09-09 16:36:12 +0000

    Initial commit.
    git-svn-id: https://rutilants.svn.sourceforge.net/svnroot/rutilants@1 68ff538a-bba1-42b7-bd40-5305963e82a6

---
+++ lib/rUtilAnts/Platform.rb
@@ -0,0 +1,30 @@
+#--
+# Copyright (c) 2009 Muriel Salvan (murielsalvan@users.sourceforge.net)
+# Licensed under the terms specified in LICENSE file. No warranty is provided.
+#++
+
+module RUtilAnts
+
+  module Platform
+
+    # OS constants
+    OS_WINDOWS = 0
+    OS_LINUX = 1
+
+    # Initialize the platform info
+    def self.initializePlatform
+      # Require the platform info
+      begin
+        require "RUtilAnts/Platforms/#{RUBY_PLATFORM}/PlatformInfo.rb"
+      rescue Exception
+        logBug "Current platform #{RUBY_PLATFORM} is not supported."
+        raise RuntimeError, "Current platform #{RUBY_PLATFORM} is not supported."
+      end
+      # Create the corresponding object
+      $CT_Platform_Info = PlatformInfo.new
+      Object.module_eval('include RUtilAnts::Platform')
+    end
+
+  end
+
+end
