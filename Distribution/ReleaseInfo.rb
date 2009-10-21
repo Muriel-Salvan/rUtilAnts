a37e61e41d3e60851339a997d1563d03391ce389
Muriel Salvan <MurielSalvan@users.sourceforge.net>
2009-10-21 10:15:34 +0000

    * Adapted distribution scripts to last release of RubyPackager.
    git-svn-id: https://rutilants.svn.sourceforge.net/svnroot/rutilants@25 68ff538a-bba1-42b7-bd40-5305963e82a6

---
+++ Distribution/ReleaseInfo.rb
@@ -0,0 +1,45 @@
+#--
+# Copyright (c) 2009 Muriel Salvan (murielsalvan@users.sourceforge.net)
+# Licensed under the terms specified in LICENSE file. No warranty is provided.
+#++
+
+$ReleaseInfo = RubyPackager::ReleaseInfo.new.
+  author(
+    :Name => 'Muriel Salvan',
+    :EMail => 'murielsalvan@users.sourceforge.net',
+    :WebPageURL => 'http://murielsalvan.users.sourceforge.net'
+  ).
+  project(
+    :Name => 'rUtilAnts',
+    :WebPageURL => 'http://rutilants.sourceforge.net/',
+    :Summary => 'A collection of various utility libraries.',
+    :Description => 'rUtilAnts is used by several projects. It includes common standard code.',
+    :ImageURL => 'http://rutilants.sourceforge.net/wiki/images/c/c9/Logo.png',
+    :FaviconURL => 'http://rutilants.sourceforge.net/wiki/images/2/26/Favicon.png',
+    :SVNBrowseURL => 'http://rutilants.svn.sourceforge.net/viewvc/rutilants/',
+    :DevStatus => 'Alpha'
+  ).
+  addCoreFiles( [
+    '{lib}/**/*'
+  ] ).
+  addAdditionalFiles( [
+    'README',
+    'LICENSE',
+    'AUTHORS',
+    'Credits',
+    'TODO',
+    'ChangeLog'
+  ] ).
+  gem(
+    :GemName => 'rUtilAnts',
+    :GemPlatformClassName => 'Gem::Platform::RUBY',
+    :RequirePath => 'lib',
+    :HasRDoc => true
+  ).
+  sourceForge(
+    :Login => 'murielsalvan',
+    :ProjectUnixName => 'rutilants'
+  ).
+  rubyForge(
+    :ProjectUnixName => 'rutilants'
+  )
